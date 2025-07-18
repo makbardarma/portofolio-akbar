ba3a91fa623d51c3bedbec5fa5af769552a8e3fd
makbardarma <peermuints@gmail.com>
2025-07-18 23:09:14 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# My Portfolio
+Selamat datang di portofolio saya! Proyek ini menampilkan keahlian, proyek, dan pengalaman saya sebagai pengembang web.
+
+
+### Weblink: [Live Website](https://yudahasibuann23.web.app/)
+## Some Images:
+<img width="450px;" src="https://github.com/rishavchanda/rishav-chanda-portfolio/assets/64485885/0a33349f-262d-4720-afed-7afc0dfd85a5"/>
+<img width="450px;" src="https://github.com/rishavchanda/rishav-chanda-portfolio/assets/64485885/92f2622f-0a7f-444a-8f60-6edeae7508dd"/>
+<img width="450px;" src="https://github.com/rishavchanda/rishav-chanda-portfolio/assets/64485885/0e984e85-cbd9-487b-bf91-166009b39319"/>
+<img width="450px;" src="https://github.com/rishavchanda/rishav-chanda-portfolio/assets/64485885/6ad7b63b-4c51-4c98-bd21-847e5d48d559"/>
+<img width="450px;" src="https://github.com/rishavchanda/rishav-chanda-portfolio/assets/64485885/10647898-7154-434c-84e6-edee6679ca92"/>
+
+ <b>INSTALL</b>
+ <p>1.npm install</p>
+<p>2.npm start</p>
+ <p>3.npm run build</p> 
+  <p>Ready to Deploy !</p>
+
+ 
+
+
+
+
+
+
+
+## Features
+- About Me: Gambaran umum tentang latar belakang, keterampilan, dan pengalaman saya.
+- Projects: Kumpulan proyek terkenal saya dengan deskripsi, tangkapan layar, dan tautan.
+- Skills: Daftar keterampilan dan kemahiran teknis saya.
+- Resume: Tautan untuk mengunduh resume saya.
+- Contact: Formulir untuk mengirimi saya pesan atau pertanyaan.
+
+## Technologies Used
+- React.js
+- HTML5 & CSS3
+- JavaScript
+- Style-Components
+- Git: A version control system for tracking changes and collaborating on projects.
+- GitHub Pages
+
+
+
---
+++ src/data/constants.js
@@ -0,0 +1,258 @@
+import projectFlowImage from '../assets/images/projectFlow.png';
+import prediksiNB from '../assets/images/prediksiNaiveBayes.png';
+import bengkelMotorCLI from '../assets/images/manajemenBengkelCLI.png';
+import ortodoxShhop from '../assets/images/ortodoxShop.png';
+import barokahAlam from '../assets/images/barokahAlam.png';
+import Inventori from '../assets/images/inventori.png';
+import Machine from '../assets/images/MACHINE.png';
+import menJamu from '../assets/images/Menjamu.png';
+import BarokahAlam from '../assets/images/barokahAlam.jpg';
+
+
+export const Bio = {
+  name: "Muchamad Akbar Darmawan",
+  roles: [
+    "Full Stack Developer",
+    "UI/UX Designer",
+    "Machine Learning Enthusiast",
+  ],
+  description:
+    "Sebagai Informatics student at Universitas Pembangunan Nasional 'Veteran' Jawa Timur,saya berkomitmen menciptakan solusi web yang intuitif, cepat, dan scalable. Lebih dari 2 tahun mengembangkan aplikasi berbasis Laravel, HTML/CSS/JS, dan MySQL. Selalu terbuka terhadap teknologi baru untuk meningkatkan kualitas setiap proyek.",
+  resume:
+    "https://drive.google.com/drive/folders/1ApccNTwG4mghzdWk-7ua9wZ_XPy7Ctr4?usp=sharing",
+  insta: "https://www.instagram.com/makbardarmaa/",
+  WhatsApp: "https://wa.me/6281283635565",
+  linkedin: "www.linkedin.com/in/makbardarma",
+  github: "https://github.com/makbardarma",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      {
+        name: "Tailwind CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Tailwind_CSS_Logo.svg/2048px-Tailwind_CSS_Logo.svg.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+    
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "Laravel",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Laravel.svg/100px-Laravel.svg.png",//ubah linknya karena tidak bisa diakses
+      },
+      {
+        name: "PHP Native",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/PHP-logo.svg/1422px-PHP-logo.svg.png",
+      },
+    ],
+  },
+  {
+    title: "UI/UX Designer",
+    skills: [
+      {
+        name: "Figma",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/3/33/Figma-logo.svg/960px-Figma-logo.svg.png",
+      }
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/git/git-original-wordmark.svg",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg",
+      }
+    ],
+  },
+];
+
+export const experiences = [
+ 
+  {
+    id: 0,
+    img: BarokahAlam,
+    role: "Frontend-Web Developer",
+    company: "CV.Barokah Alam",
+    date: "Apr 2025 - June 2025",
+    desc: "Menggunakan HTML, Boostrap, dan JavaScript . Di frontend aplikasi web yang di gunakan, serta PHP untuk mengelola navigasi Halaman , untuk mengelola interaksi pengguna dan membuat aplikasi web yang responsif.",
+    skills: [
+      "HTML",
+      "Bootstrap",
+      "JavaScript",
+      "PHP",
+      "JavaScript",
+    ],
+  },
+  {
+    id: 1,
+    img: "https://simbelmawa.kemdikbud.go.id/pkm/wp-content/uploads/2025/07/Logo-PKM-Warna.png",
+    role: "UI/UX Website - PKM Menjamu 2025",
+    company: "Faculty of Engineering & Science",
+    date: "Apr 2025 - June 2025",
+    desc: "Peran desain antarmuka pengguna (UI) melibatkan penciptaan dan perancangan elemen visual serta tata letak produk digital atau aplikasi. Desainer UI berfokus pada pembuatan antarmuka yang ramah pengguna dan menarik secara visual untuk meningkatkan pengalaman pengguna. Tugas mereka meliputi perancangan tombol, menu, ikon, dan estetika keseluruhan untuk memastikan pengguna dapat berinteraksi dengan produk secara mudah dan intuitif. Pada dasarnya, desain UI memainkan peran krusial dalam membuat antarmuka digital yang menarik secara visual, fungsional, dan berpusat pada pengguna.",
+    skills: [
+      "UI/UX",
+      "Figma",
+    ],
+  },
+  
+  {
+    id: 2,
+    img: "https://himatifa.org/assets/images/logo.png",
+    role: "Head of Public Relations PEMIRA IF 2025",
+    company: "BEM FASILKOM UPN Veteran Jawa Timur",
+    date: "Sep 2024 - Jan 2025",
+    desc: "Melalui peran strategis di Divisi Pengabdian Masyarakat, saya secara aktif mengaplikasikan dan mengasah berbagai keterampilan. Saya mempraktikkan manajemen proyek dan perencanaan acara dengan mengelola kegiatan dari konsep hingga selesai. Kemampuan kerja tim dan komunikasi menjadi kunci dalam berkolaborasi dengan anggota divisi dan mitra eksternal. Selain itu, saya juga berkontribusi pada proyek teknis yang menantang kemampuan problem-solving saya dalam konteks sosial yang nyata.",
+    skills: [
+      "Kepemimpinan",
+      "Public Speaking",
+      "Manajemen Waktu",
+      "Kerja Tim",
+      "Pemecahan Masalah",
+      "Komunikasi",
+      "Pemikiran Kritis",
+    ],
+   
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsDwiXIU7GM1RJs8LnvrCNdkjMpnIblvLAZQ&s",
+    school: "Universitas Pembangunan Nasional 'Veteran' Jawa Timur",
+    date: "2023 - 2027 (Semester-5)",
+    grade: "3.86 Index Prestasi",
+    desc: "Saat Ini saya lagi melanjutkan pendidikan di Universitas Pembangunan Nasional 'Veteran' Jawa Timur dengan jurusan Informatika (Semester 5). Saya telah mempelajari berbagai konsep dan teknologi dalam bidang pengembangan perangkat lunak, termasuk pemrograman web, basis data, dan desain antarmuka pengguna. Saya juga aktif terlibat dalam proyek-proyek pengembangan aplikasi dan berpartisipasi dalam kegiatan organisasi kampus.",
+    degree: "Informatika",
+  },
+  {
+    id: 1,
+    img: "https://sman1krembung.sch.id/wp-content/uploads/2024/12/1000594767-fotor-bg-remover-20241204234251.png",
+    school: "SMA Negeri 1 KREMBUNG",
+    date: "2020 - 2023",
+    grade: "92.89%",
+    desc: "Saya telah menyelesaikan pendidikan di SMA Negeri 1 Krembung Sidoarjo , jurusan Matematika dan Ilmu Pengetahuan Alam (MIPA) . Selama menempuh pendidikan di jurusan MIPA, saya mempelajari dasar-dasar Matematika, Fisika, Kimia, dan Biologi yang mengasah kemampuan logika, analisis, dan pemecahan masalah. Saya juga mendapat pengenalan terhadap teknologi seperti pemrograman, basis data, dan desain antarmuka, serta aktif dalam proyek aplikasi dan kegiatan organisasi sekolah.",
+    degree: "Matematika dan Ilmu Pengetahuan Alam (MIPA)",
+  },
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "CV Barokah Alam",
+    date: "Mei 2025",
+    description:
+      "Aplikasi ini merupakan aplikasi E-commerce atau website penjualan CV. Barokah Alam yang berlokasi di kota Bojonegoro, Website e-commerce ini dikembangkan untuk CV.Barokah Alam, sebuah usaha dagang yang bergerak di bidang distribusi dan penjualan beras lokal berkualitas. Platform ini dirancang untuk memudahkan pelanggan dalam melakukan pemesanan beras secara online, baik dalam partai kecil maupun besar.",
+    image:
+      barokahAlam,
+    tags: [
+
+      "PHP Native",
+      "Java Script",
+      "MySQL",
+      "HTML",
+      "Bootstrap",
+    ],
+    category: "web app",
+    github: "https://github.com/makbardarma",
+    webapp: "https://github.com/makbardarma",
+  },
+  {
+    id: 1,
+    title: "Project Inventori Supermarket",
+    date: "Mei 2025",
+    description:
+      "Aplikasi ini merupakan aplikasi web Project Flow, ToDoList, untuk start-up Kreotek Inova Digital. Aplikasi ini dibuat menggunakan React JS, Tailwind CSS, dan Node JS. Aplikasi ini juga dilengkapi dengan fitur login, register, dan dashboard untuk mengelola proyek. Aplikasi ini juga dilengkapi dengan fitur notifikasi untuk memudahkan pengguna dalam mengetahui status proyek.",
+    image: Inventori,
+    tags: ["PHP Native", "Javascript", "Css", "Bootstrap", "HTML","MySQL"],
+    category: "web app",
+    github: "https://github.com/makbardarma",
+    webapp: "https://github.com/makbardarma",
+  },
+  {
+    id: 2,
+    title: "Fuzzy C-Means Clustering Udang Vaname",
+    date: "Mei 2025",
+    description:
+      "Aplikasi ini menggunakan algoritma Fuzzy C-Means untuk melakukan proses clustering pada data budidaya udang vaname. Tujuannya adalah untuk mengelompokkan data berdasarkan kemiripan karakteristik, sehingga dapat membantu dalam analisis kualitas, pertumbuhan, dan kondisi lingkungan tambak. Aplikasi ini juga dilengkapi dengan visualisasi data untuk mempermudah interpretasi hasil clustering, dan cocok digunakan dalam pengambilan keputusan berbasis data di sektor akuakultur.",
+    image: Machine,
+    tags: ["Python", "Fuzzy C-Means", "Pandas", "NumPy", "Google Colab", "Seaborn", "Scikit-learn"],
+    category: "machine learning",
+    github: "https://github.com/makbardarma",
+    webapp: "https://colab.research.google.com/drive/1N0-JkAroJB3gV0XZ_uOlileSrnhJHyXR",
+  },
+  {
+    id: 3,
+    title: "PKM Menjamu 2025 ",
+    date: "Mei 2025",
+    description:
+      "Dengan pendekatan UI/UX yang intuitif dan memikat Web E-commerce Menjamu 2025 dirancang untuk menciptakan pengalaman pengguna yang mulus dan menyenangkan. Hal ini akan memudahkan pengguna dalam menemukan dan membeli produk organik berkualitas sekaligus memaksimalkan pemanfaatan fitur app builder",
+    image:
+      menJamu,
+    tags: ["Figma", "Wareframe", "Mockup", "Prototype","Desain Thinking"],
+    category: "UI/UX desain",
+    github: "https://github.com/makbardarma",
+    webapp: "https://www.figma.com/design/jTannLxcYSw2PosDyvrXk4/PKM-2025?node-id=2-7&t=E7wkVHTnUoMptrIi-1",
+  },
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
